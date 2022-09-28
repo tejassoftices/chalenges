779826f83bef62143ad53517cfced5d5b5f3c30e
tejasraut <tejasraut@softices.com>
2022-09-28 10:41:59 +0530

    newly modified

--- basicExr/basic1.rb
+++ basicExr/basic1.rb
@@ -1,5 +1,6 @@
 # 1}Write a Ruby program to create a new string
 # which is n copies of a given string where n is a non-negative integer.
+
 # def multiple_string (str, n)
 #     return str * n
 # end
@@ -25,18 +26,29 @@
 
 # #3}weather a string starts with if
 # def start_if(str)
-#     return str[0,2] == "if";
+  # return str[0,2] == "if";
+  
+  # if str[0,2] == "if"
+  #   return true
+  # else
+  #   return false
+  # end
 # end
 # puts start_if("ifelse")
-# puts start_if("elseif")
-#
+# puts start_if("ifthrow")
+
+
+
 # 4}print first and last name in a reverse order
+
 # puts "inputs your fname"
 # fname = gets.chomp
 # puts "input your last name"
 # lname = gets.chomp
 # puts "hello #{lname} #{fname}"
-#
+
+#5}return file extension
+
 # def extention(str)
 #     fbname = File.basename str
 #     puts "file name: " +fbname
@@ -45,7 +57,9 @@
 # end
 # puts extention("base.rb")
 
-# #get array from user and print it
+
+#6}get array from user and print it
+
 # i = 10
 # a = []
 # while i>0
@@ -56,11 +70,11 @@
 # end
 # puts "#{a}"
 
-# Write a Ruby program to check three numbers and return true if one or more of them
+#7} Write a Ruby program to check three numbers and return true if one or more of them
 # are small. A number is called "small" if it is in the range 1..10 inclusive.
 
 def in110(a, b, c)
-  ((a >= 1 && a <= 10) || (b >= 10 && b <= 10) || (c >= 1 && c <= 10))
+  ((a >= 1 && a <= 10) || (b >= 1 && b <= 10) || (c >= 1 && c <= 10))
 end
 print in110(1, 2, 3), "\n"
 print in110(10, 15, 33), "\n"
--- exercises3.5/e12_file.rb
+++ exercises3.5/e12_file.rb
@@ -1,19 +1,20 @@
-# write a program to get a list of all files/directories from the given
+#write a program to get a list of all files/directories from the given
 
 class List
-  def get_file_list(path)
-    Dir.foreach("#{path}") do |entry|
-      puts entry
+    def get_file_list(path)
+        Dir.foreach("#{path}") do |entry|
+            puts entry
+        end
     end
-  end
-
-  def printFilepath(str)
-    puts str
-  end
-end
+    def printFilepath(str)
+        puts str
+    end
+end 
 list = List.new
-puts 'Enter a file path:'
+puts "Enter a file path:"
 str = gets.chomp.to_s
 list.get_file_list(str)
+    
+
 
 # /home/scpl/Desktop/workspace/ruby_advance
